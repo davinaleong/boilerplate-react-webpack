d8777291155059b08cf3abfe39912dfdbc44501e
Davina Leong <leong.shi.yun@gmail.com>
2023-04-04 16:23:58 +0800

    Added react refresh

--- README.md
+++ README.md
@@ -9,3 +9,23 @@ I'm also planning to add SASS and environment-specific configs to this boilerpla
 ## Stopped At
 
 https://www.youtube.com/watch?v=25kFcF54GNk&list=PLC3y8-rFHvwiWPS2RO3BKotLRfgg_8WEo&index=3
+
+## How to
+
+1. Install dependencies
+
+```
+cd learn-react-webpack && npm install
+```
+
+2. Run development server
+
+```
+npm start
+```
+
+3. Build package
+
+```
+npm run build
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './main.css'
+
 import HTML5_LOGO from './images/html5-png.png'
 import CSS3_LOGO from './images/css3-svg.svg'
+
+import { CounterComponent } from './components/counter.component'
 
 export const App = () => {
     return (
         <>
-            <h1>React Typescript Webpack Boilerplate</h1>
+            <h1>Hello React Typescript Webpack Boilerplate</h1>
+            <p>Environment: <strong>{process.env.NODE_ENV}</strong></p>
+
+            <CounterComponent />
 
             <p><img src={HTML5_LOGO} alt="html5 logo" width="512" height="512"/></p>
             <p><img src={CSS3_LOGO} alt="css3 logo" width="512" height="512"/></p>
