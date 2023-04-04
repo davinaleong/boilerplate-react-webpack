11530586f041f45b4ada439e9103b7ec2d78e09b
Davina Leong <leong.shi.yun@gmail.com>
2023-04-04 14:41:24 +0800

    Moves images into containing folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './main.css'
-import html5Img from './html5-png.png'
-import css3Img from './css3-svg.svg'
+import HTML5_LOGO from './images/html5-png.png'
+import CSS3_LOGO from './images/css3-svg.svg'
 
 export const App = () => {
     return (
         <>
             <h1>React Typescript Webpack Boilerplate</h1>
 
-            <p><img src={html5Img} alt="html5 logo" width="512" height="512"/></p>
-            <p><img src={css3Img} alt="css3 logo" width="512" height="512"/></p>
+            <p><img src={HTML5_LOGO} alt="html5 logo" width="512" height="512"/></p>
+            <p><img src={CSS3_LOGO} alt="css3 logo" width="512" height="512"/></p>
         </>
     )
 }
