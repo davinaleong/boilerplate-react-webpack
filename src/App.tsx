530e43c76c5b559f69477ab696fe6d096875409e
Davina Leong <leong.shi.yun@gmail.com>
2023-04-04 14:39:52 +0800

    Imported image assets and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import './main.css'
+import html5Img from './html5-png.png'
+import css3Img from './css3-svg.svg'
+
 export const App = () => {
-    return <h1>React Typescript Webpack Boilerplate</h1>
+    return (
+        <>
+            <h1>React Typescript Webpack Boilerplate</h1>
+
+            <p><img src={html5Img} alt="html5 logo" width="512" height="512"/></p>
+            <p><img src={css3Img} alt="css3 logo" width="512" height="512"/></p>
+        </>
+    )
 }
