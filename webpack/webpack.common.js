01f9f686823f76535bc3ceb3c7eae61270be16f0
Davina Leong <leong.shi.yun@gmail.com>
2023-05-07 21:22:24 +0800

    Added environment plugin

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const Webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 // const CopyPlugin = require('copy-webpack-plugin')
 
@@ -37,6 +38,9 @@ module.exports = {
     filename: 'bundle.js',
   },
   plugins: [
+    new Webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
